126ec3f3e79cec8454320281cc69309a5f021a02
icuthy <icuthy@gmail.com>
2019-01-03 19:26:27 -0800

    Specify default props

--- src/Loader.js
+++ src/Loader.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-const Loader = () => {
+const Loader = (props) => {
     return (
         <div className='ui active dimmer'>
-            <div className='ui bigtext loader'>
-                Loading!
+            <div className='ui big text loader'>
+                {props.message}
             </div>
         </div>
     )
-
 }
+// Way to provide default props, could also use { props.message || 'Loading...'} in function above
+Loader.defaultProps = {
+    message: 'Loading...'
+};
+
 export default Loader;
