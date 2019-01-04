35b6aeb28e11f25a2e28c2892524dac4dcd555b8
icuthy <icuthy@gmail.com>
2019-01-03 19:12:45 -0800

    Style season display and create loader component for improving UX

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './SeasonDisplay.css'
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -26,10 +26,10 @@ const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
     const { text, iconName } = seasonConfig[season];
     return (
-        <div>
-            <i className={`${iconName} icon`}></i>
+        <div className={`SeasonDisplay ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}></i>
             <h1>{text}</h1>
-            <i className={`${iconName} icon`}></i>
+            <i className={`icon-right massive ${iconName} icon`}></i>
         </div>
     )
 }
