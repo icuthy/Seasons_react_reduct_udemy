4e9375aabee29e9a69cfdd4c84dcaaf893a052fe
icuthy <icuthy@gmail.com>
2019-01-03 17:42:49 -0800

    Add SeasonDisplay component to index and add getting month and displaying season logic to component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    // if only showing state and no other methods in constructor, can remove it and use shorthand which babel compiles into contstructor with super(props) etc. Can test out in Babel.io
-    // constructor(props) {
-    //     super(props); 
-    //     this.state = { lat: null, errorMessage: ''};
-    // }
-
     state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
@@ -15,20 +10,16 @@ class App extends React.Component {
             position => this.setState({ lat: position.coords.latitude }),
             err => this.setState({ errorMessage: err.message })
         );
-    }
-    componentDidUpdate() {
-        console.log('My component was just updated- it rerendered');
 
     }
 
-    // react says we have to define render
     render() {
 
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return <div>Loading!</div>
