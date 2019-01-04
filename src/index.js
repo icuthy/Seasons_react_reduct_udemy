6d4d3a79765f48212b61a942422b2c25d70a7757
icuthy <icuthy@gmail.com>
2019-01-03 17:40:38 -0800

    Add geolocation api call to componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,43 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// functional component used for simple routines
-// const App = () => {
-//     window.navigator.geolocation.getCurrentPosition(
-//         (position) => console.log(position),
-//         (err) => console.log(err)
-//     );
-//     return (
-//         <div>Latitude: </div>
-
-//     )
-// }
-
-// Refactor to class when more complex or need to use STATE (like we need to do in this case so that we can rerender the jsx after the result of the geolocation api comes in) Our App class is extending or borrowing like a subclass all the methods from React.Component class.
 
 class App extends React.Component {
-    // es6 method constructor always called first in component
-    constructor(props) {
-        super(props); // reference to React.Components parent's constructor function
+    // if only showing state and no other methods in constructor, can remove it and use shorthand which babel compiles into contstructor with super(props) etc. Can test out in Babel.io
+    // constructor(props) {
+    //     super(props); 
+    //     this.state = { lat: null, errorMessage: ''};
+    // }
 
-        // this is the ONLY time we do direct assignment to this.state, initializing state object
-        this.state = { lat: null, errorMessage: '' };
-
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // call setState to update state!!!
-                this.setState({ lat: position.coords.latitude });
-
-                // do not do  this.setState.lat = { position.coords.latitude }
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-                console.log(err);
-            }
-        );
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
-        console.log('My component was rendered to the screen');
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
     }
     componentDidUpdate() {
         console.log('My component was just updated- it rerendered');
